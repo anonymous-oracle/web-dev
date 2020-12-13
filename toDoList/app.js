986e4947c9864b187d2ed4efee40994cf21cb7eb
Suhas KS <suhass149@gmail.com>
2020-12-13 23:10:57 +0530

    started toDoList

--- toDoList/app.js
+++ toDoList/app.js
@@ -2,3 +2,20 @@ const express = require('express');
 const request = require('request');
 const https = require('https');
 
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hi!');
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){console.log(__filename+' running on PORT 3000')});
