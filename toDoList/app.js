4f49cec2015c10ef5e32c296e6e09b5e3a7e5255
Suhas KS <suhass149@gmail.com>
2020-12-14 20:04:44 +0530

    update

--- toDoList/app.js
+++ toDoList/app.js
@@ -3,15 +3,58 @@ const request = require('request');
 const https = require('https');
 
 const app = express();
+app.use(express.urlencoded({extended: true}));
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItems = [];
 
+app.use(express.static('public'));
+
+// using ejs templating
+// NOTE: for ejs templating use this project as a tutorial guide
+app.set('view engine', 'ejs');
+
+
+
+// once the views folder is created with all templates, the files inside should be mentioned using filepaths relative to the views folder
+let listItems = ['task 1', 'task 2', 'task 3'];
 app.get('/', function (req, res) {
-    res.send('Hi!');
+    let today = new Date();
+    let day;
+    // let currentDay = today.getDay();
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    day = today.toLocaleDateString('en-US', options);
+
+    res.render('list', {listTitle:day, newListItems: listItems});
 });
 
+app.post('/', (req, res)=> {
+    if (req.body.list === 'Work'){
+        workItems.push(req.body.newItem);
+        res.redirect('/work');
+    } else {
+        listItems.push(req.body.newItem);
+        res.redirect('/');
+    }
+});
 
+app.get('/work', function(req, res){
+    res.render('list', {listTitle: 'Work List', newListItems: workItems});
+});
 
+app.post('/work', function(req, res){
+    workItems.push(req.body.newItem);
+    res.redirect('/work');
+})
 
-
+app.get('/about', function(req, res){
+    res.render('about');
+})
 
 
 
